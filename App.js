6e811cf3930c2ea51fcd968f844a94ff7d69e377
alexanderMontague <xturnermonty@gmail.com>
2018-12-01 12:13:12 -0500

    Add upload page

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { StyleSheet, View } from "react-native";
 
 import Layout from "./components/Layout";
+import UploadPage from "./components/UploadPage";
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Layout />
+        {/* <Layout /> */}
+        <UploadPage />
       </View>
     );
   }
---
+++ components/UploadPage.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { View, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
+
+import TeamDeew from "../assets/TeamDEEW.png";
+
+export default class Homescreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <Text style={styles.title}> Classes With Friends! </Text>
+          <Image source={TeamDeew} style={styles.image} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={this.onPress}>
+            <Text> Add Cladsfgsdfgfsdg ss Schedule </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={this.onPress}>
+            <Text> Enter Friend's Code </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 10
+  },
+  image: {
+    height: 150,
+    width: 150
+  },
+  topContainer: {
+    //flex: 1,
+    //marginTop: 100,
+    //justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonContainer: {
+    //flex: 1,
+    //justifyContent: "center"
+    //paddingHorizontal: 10
+  },
+  button: {
+    margin: 10,
+    backgroundColor: "rgba(151, 213,200, 1)",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 20
+  },
+  title: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#fff"
+  }
+});
